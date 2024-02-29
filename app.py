3404faf22e2dcbe483bd0154fbaff92c7e791c4e
Yukesh P <112232878+yukesh3111@users.noreply.github.com>
2024-02-29 14:19:57 +0000

    resource are uploaded and templates are ready

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 app=Flask(__name__)
 @app.route('/')
 def helloworld():
-  return "Hello Yukesh"
+  return render_template("index.html")
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
